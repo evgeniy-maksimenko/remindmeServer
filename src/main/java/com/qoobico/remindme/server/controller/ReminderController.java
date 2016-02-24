c210922f7e8c6b5e0b8be215eb599de5ee3be070
magoni <e.maksimenko@gmail.com>
2016-02-24 09:15:21 +0200

    add spring repository

--- src/main/java/com/qoobico/remindme/server/controller/ReminderController.java
+++ src/main/java/com/qoobico/remindme/server/controller/ReminderController.java
@@ -1,6 +1,8 @@
 package com.qoobico.remindme.server.controller;
 
 import com.qoobico.remindme.server.entity.Remind;
+import com.qoobico.remindme.server.repository.RemindRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,14 +11,19 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Date;
+import java.util.List;
 
 @RestController
 @RequestMapping("/reminder")
 public class ReminderController {
 
+    @Autowired
+    private RemindRepository remindRepository;
+
     @RequestMapping(value = "/get", method = RequestMethod.GET)
     @ResponseBody
     public Remind getReminder() {
+        List<Remind> list = remindRepository.findAll();
         return createMockRemind();
 
     }
